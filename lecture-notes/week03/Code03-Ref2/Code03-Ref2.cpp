496bf0ce2ef7b2da37887558c5fac9d67a04d512
dapin <89961651+dapin1490@users.noreply.github.com>
2022-09-21 10:37:52 +0900

    Update Code03-Ref2.cpp

--- lecture-notes/week03/Code03-Ref2/Code03-Ref2.cpp
+++ lecture-notes/week03/Code03-Ref2/Code03-Ref2.cpp
@@ -1,116 +1,116 @@
 #include <iostream>
 using namespace std;
 
-class node
-{
+class node {
 public:
 	int data;
 	node *left;
 	node *right;
 };
-class bst
-{
+
+class bst {
 private:
-	node *find_impl(node *current, int value)
-	{
-		if (!current)
-		{
+	node *find_impl(node *current, int value) {
+		if (!current) {
 			cout << endl;
 			return NULL;
 		}
-		if (current->data == value)
-		{
+
+		if (current->data == value) {
 			cout << value << "has been found." << endl;
 			return current;
 		}
-		if (value < current->data)
-		{
+
+		if (value < current->data) {
 			cout << current->data << "moved to left.";
 			return find_impl(current->left, value);
 		}
+
 		cout << current->data << "moved to right.";
 		return find_impl(current->right, value);
 	}
-	void insert_impl(node *current, int value)
-	{
-		if (value < current->data)
-		{
+
+	void insert_impl(node *current, int value) {
+		if (value < current->data) {
 			if (!current->left)
 				current->left = new node{value, NULL, NULL};
 			else
 				insert_impl(current->left, value);
 		}
-		else
-		{
+		else {
 			if (!current->right)
 				current->right = new node{value, NULL, NULL};
 			else
 				insert_impl(current->right, value);
 		}
 	}
-	void inorder_impl(node *start)
-	{
+
+	void inorder_impl(node *start) {
 		if (!start)
 			return;
+
 		inorder_impl(start->left);
 		cout << start->data << " ";
 		inorder_impl(start->right);
 	}
-	node *delete_impl(node *start, int value)
-	{
+
+	node *delete_impl(node *start, int value) {
 		if (!start)
 			return NULL;
+
 		if (value < start->data)
 			start->left = delete_impl(start->left, value);
 		else if (value > start->data)
 			start->right = delete_impl(start->right, value);
-		else
-		{
-			if (!start->left)
-			{
+		else {
+			if (!start->left) {
 				auto tmp = start->right;
 				delete start;
 				return tmp;
 			}
-			if (!start->right)
-			{
+
+			if (!start->right) {
 				auto tmp = start->left;
 				delete start;
 				return tmp;
 			}
+
 			auto succNode = successor(start);
 			start->data = succNode->data;
 			start->right = delete_impl(start->right, succNode->data);
 		}
+
 		return start;
 	}
 
 public:
 	node *root = nullptr;
-	node *find(int value)
-	{
+	
+	node *find(int value) {
 		return find_impl(root, value);
 	}
-	void insert(int value)
-	{
+
+	void insert(int value) {
 		if (!root)
 			root = new node{value, NULL, NULL};
 		else
 			insert_impl(root, value);
 	}
-	void inorder()
-	{
+
+	void inorder() {
 		inorder_impl(root);
 	}
-	node *successor(node *start)
-	{
+
+	node *successor(node *start) {
 		auto current = start->right;
+
 		while (current && current->left)
 			current = current->left;
+
 		return current;
 	}
-	void deleteValue(int value)
-	{
+
+	void deleteValue(int value) {
 		root = delete_impl(root, value);
 	}
 };
@@ -118,6 +118,7 @@ public:
 int main()
 {
 	bst tree;
+
 	tree.insert(12);
 	tree.insert(10);
 	tree.insert(20);
