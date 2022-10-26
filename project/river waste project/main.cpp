ea461b19ce1e2cd6c43ab49a1999231d5ce431fd
dapin <dapin1490@gmail.com>
2022-10-26 21:49:02 +0900

    221026 프로젝트
    메소드 약간 추가, 시스템 클래스 만들기만 함

--- project/river waste project/main.cpp
+++ project/river waste project/main.cpp
@@ -1,43 +1,60 @@
 #include <iostream>
 #include <vector> // vector
 #include <utility> // pair
-#include <algorithm> // copy
+#include <algorithm> // copy, sort(예정)
 using namespace std;
-
-class accumulate_node { // 누적 민원 노드 클래스
-private:
-	int waste_number; // 쓰레기 분류 번호
-	int accmed_compls; // 누적 민원 수
-	vector<complain_node> compl_list; // 해당 민원 벡터
-
-public:
-	/* 생성자는 프로그램 첫 실행 시 사용자의 조작 없이 자동으로 정해진 수만큼 실행되게 할 것.
-	민원이 하나도 들어오지 않은 상태를 전제로 한다. */
-	accumulate_node(int wn);
-	/*
-	필요 메소드
-	+ 누적 민원 수 변경 메소드
-	+ 쓰레기 분류 번호 반환 메소드
-	+ 누적 민원 수 반환 메소드
-	+ 민원 벡터 반환 메소드
-	*/
-};
 
 class complain_node { // 민원 노드 클래스
 private:
 	string pic_name; // 사진 파일명
 	pair<int, int> pic_size; // 사진 크기
 	pair<double, double> coordinates; // 좌표
-	int wastes[5]; // 쓰레기 유무 표시
 
 public:
+	int wastes[5]; // 쓰레기 유무 표시
+
 	complain_node(string pn, int width, int height, double x, double y, int ws[5]);
 
-	/*
-	필요 메소드
-	+ 각 멤버 변수별 값을 반환하는 메소드들
-	+ 쓰레기 유무 배열 변경 메소드
-	*/
+	// 각 멤버 변수별 값을 반환하는 메소드들
+	string get_name() { return pic_name; }
+	pair<int, int> get_size() { return pic_size; }
+	pair<int, int> get_codi() { return coordinates; }
+};
+
+class accumulate_node { // 누적 민원 노드 클래스
+private:
+	int waste_number; // 쓰레기 분류 번호
+	vector<complain_node> compl_list; // 해당 민원 벡터
+
+public:
+	/* 생성자는 프로그램 첫 실행 시 사용자의 조작 없이 자동으로 정해진 수만큼 실행되게 할 것.
+	민원이 하나도 들어오지 않은 상태를 전제로 한다. */
+	accumulate_node(int wn);
+
+	// 새 민원 추가됨
+	void add_compl();
+
+	// 누적 민원 처리
+	void clear_compl();
+
+	// 쓰레기 분류 번호 반환
+	int get_wnum() { return waste_number; }
+
+	// 누적 민원 수 반환
+	int get_num_of_compls() { return compl_list.size(); }
+
+	// 민원 벡터 반환
+	vector<complain_node> get_compl_list() { return compl_list; }
+};
+
+class waste_system { // 하천 쓰레기 민원 처리 시스템
+private:
+	accumulate_node acc_list[5]; // 누적 민원 객체 배열
+	int area; // 담당 지역 번호(행정동코드 사용, 사용자가 시스템 생성 시 직접 입력)
+
+
+public:
+	// 생성자 등등 추가
 };
 
 int main() {
@@ -49,7 +66,6 @@ int main() {
 
 accumulate_node::accumulate_node(int wn) {
 	waste_number = wn;
-	accmed_compls = 0;
 }
 
 complain_node::complain_node(string pn, int width, int height, double x, double y, int ws[5]) {
