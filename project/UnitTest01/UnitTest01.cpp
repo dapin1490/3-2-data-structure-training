3fed7781953738c3f4f64b86ae816d99f9d19386
dapin <dapin1490@gmail.com>
2022-11-02 00:14:52 +0900

    프로젝트에 이것저것 채움
    프로젝트 순서도 그렸음, 행정동코드 파일 추가, 테스트용 서울특별시 종로구 데이터파일("1111000000.csv", 비어 있음) 추가, compl_system::system_on 구현하다 말았음, 일부 주석 추가. 유닛테스트 약간 바꿨지만 테스트가 추가된 건 아니고 만들다 말았음(사용자 입력을 자동으로 대체하지 못해 못만들었음)

--- project/UnitTest01/UnitTest01.cpp
+++ project/UnitTest01/UnitTest01.cpp
@@ -10,6 +10,25 @@ namespace UnitTest01
 	TEST_CLASS(UnitTest01)
 	{
 	public:
+		TEST_METHOD_INITIALIZE(Initialize)
+		{
+			// method initialization code
+			/*
+			각 테스트 메서드를 실행하기 전에 실행되는 메서드로 methodName을 정의합니다.
+			TEST_METHOD_INITIALIZE는 테스트 클래스에서 한 번만 정의할 수 있으며
+			테스트 클래스의 범위에서 정의해야 합니다.
+			*/
+		}
+
+		TEST_METHOD_CLEANUP(Cleanup)
+		{
+			// test method cleanup code
+			/*
+			각 테스트 메서드를 실행한 후에 실행되는 메서드로 methodName을 정의합니다.
+			TEST_METHOD_CLEANUP는 테스트 클래스에서 한 번만 정의할 수 있으며
+			테스트 클래스의 범위에서 정의해야 합니다.
+			*/
+		}
 		
 		TEST_METHOD(BasicConstructorNMethod)
 		{
@@ -25,6 +44,12 @@ namespace UnitTest01
 			Assert::AreEqual(string("test2.jpg"), c2.get_name());
 			Assert::AreEqual(0, a1.get_compls_size());
 			Assert::AreEqual(0, a1.get_num());
+		}
+
+		TEST_METHOD(SystemOnTest)
+		{
+			// test 2: 시스템 실행 시 지역코드 입력 후 파일 확인
+
 		}
 	};
 }
