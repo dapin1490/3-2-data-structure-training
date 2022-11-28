34602b842776a23faefd17249b96935f75bf14e6
dapin <dapin1490@gmail.com>
2022-11-28 21:27:40 +0900

    필요 없어서 지움

--- project/river_waste_project_py/main.py
+++
@@ -1,8 +0,0 @@
-import compl_sys_module as csys
-
-if __name__ == "__main__":
-	print("excute main.py")
-	csys.fout.write(f"main.py 실행 시각 {csys.currentDateTime()}\n\n")
-
-	csys.fin.close()
-	csys.fout.close()
